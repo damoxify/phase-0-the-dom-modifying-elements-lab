aacef01b69fab641454f1808f128bf6cae5159a1
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-24 16:50:23 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const afro = document.querySelector("main")
+afro.remove();
+
+const newHeader = document.createElement("h1");
+const yourName = 'YOUR-NAME'
+newHeader.id = "victory"
+newHeader.textContent = `${yourName} is the champion`;
+
+
